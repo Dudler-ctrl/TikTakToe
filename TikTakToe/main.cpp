d1ea0d16d4bed797de44eda40f66b6b1ec703f7c
aiusz <kuboszek.arkadiusz@gmail.com>
2020-01-14 17:53:39 +0100

    Dodalismy funkcje move

--- TikTakToe/main.cpp
+++ TikTakToe/main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
+//using namespace std;
+//
+//int pomnoz(int a, int b){
+//    return a*b;
+//}
+//
+//int dodaj(int a, int b){
+//    return a+b;
+//}
+//
+//double podziel(int a, int b){
+//    return ((double)a)/b;
+//}
+//
+//void zad6(int n){
+//    for (int a = 1; a<n; a++){
+//        cout << "(" << a << ", " << n-a << "), ";
+//    }
+//}
 
-using namespace std;
 
-
-int pomnoz(int a, int b){
-    return a*b;
+bool move(int x, int y, char sign){
+    if (x<0 or x >2 or y < 0 or y >2 ){
+        return false;
+    }
+    if (tab[x][y] != ' '){
+        return false;
+    }
+    tab[x][y] = sign;
 }
 
-int dodaj(int a, int b){
-    return a+b;
-}
-
-double podziel(int a, int b){
-    return ((double)a)/b;
-}
 
 int main()
 {
+    int n;
+    cin >> n;
+    zad6(n);
 
     int a, b;
     cout << "Podaj a: " <<endl;
