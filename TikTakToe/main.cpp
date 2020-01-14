256e525ed27f94bb0ecb4cf9111c4565c52aa594
aiusz <kuboszek.arkadiusz@gmail.com>
2020-01-14 17:17:41 +0100

    Dodalem funckje dzielaca 2 liczby

--- TikTakToe/main.cpp
+++ TikTakToe/main.cpp
@@ -11,11 +11,24 @@ int dodaj(int a, int b){
     return a+b;
 }
 
+double podziel(int a, int b){
+    return ((double)a)/b;
+}
+
 int main()
 {
 
-    cout << pomnoz(4,10) <<endl;
-    cout<<dodaj(3,5)<<endl;
+    int a, b;
+    cout << "Podaj a: " <<endl;
+    cin >> a;
+    cout << "Podaj b: " <<endl;
+    cin >> b;
+
+    cout << pomnoz(a,b) <<endl;
+    cout << podziel(a,b) <<endl;
+    cout<<dodaj(a,b)<<endl;
     cout << "Inny komunikat!" << endl;
+
+
     return 0;
 }
