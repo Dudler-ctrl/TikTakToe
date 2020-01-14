525b6b9a9256a034500d97d09cf9247b8c695589
Dudler-ctrl <kamildudzik101@gmail.com>
2020-01-14 17:18:50 +0100

    Dodalem funkcje odejmij

--- TikTakToe/main.cpp
+++ TikTakToe/main.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+int odejmij(int a, int b){
+    return a-b;
+}
 
 int pomnoz(int a, int b){
     return a*b;
@@ -16,6 +19,7 @@ int main()
 
     cout << pomnoz(4,10) <<endl;
     cout<<dodaj(3,5)<<endl;
+    cout<<odejmij(12,8)<<endl;
     cout << "Inny komunikat!" << endl;
     return 0;
 }
