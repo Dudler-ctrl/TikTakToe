9212223db3434f030e4a70a3f447f04fefb7430a
aiusz <kuboszek.arkadiusz@gmail.com>
2020-01-14 17:08:27 +0100

    Dodalem funkcje pomnoz

--- TikTakToe/main.cpp
+++ TikTakToe/main.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
+int pomnoz(int a, int b){
+    return a*b;
+}
+
 int main()
 {
+
+    cout << pomnoz(4,10) <<endl;
     cout << "Inny komunikat!" << endl;
     return 0;
 }
