92b5c2b0cc9df1818809a69ec2a258704a18af20
Dudler-ctrl <kamildudzik101@gmail.com>
2020-01-14 17:06:20 +0100

    dodalem funckje dodajaca 2 liczby

--- TikTakToe/main.cpp
+++ TikTakToe/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
+int dodaj(int a, int b){
+    return a+b;
+}
 int main()
 {
+    cout<<dodaj(3,5)<<endl;
     cout << "Inny komunikat!" << endl;
     return 0;
 }
