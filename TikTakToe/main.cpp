7d8d2b7b57fccac572f577e547f12da364c4a2cb
Arkadusz <arkadusz.kuboszek@gmail.com>
2020-01-21 18:22:19 +0100

    Kolko i krzyzyk prawie dzialajace

--- TikTakToe/main.cpp
+++ TikTakToe/main.cpp
@@ -1,53 +1,96 @@
 #include <iostream>
 
-//using namespace std;
-//
-//int pomnoz(int a, int b){
-//    return a*b;
-//}
-//
-//int dodaj(int a, int b){
-//    return a+b;
-//}
-//
-//double podziel(int a, int b){
-//    return ((double)a)/b;
-//}
-//
-//void zad6(int n){
-//    for (int a = 1; a<n; a++){
-//        cout << "(" << a << ", " << n-a << "), ";
-//    }
-//}
+using namespace std;
 
 
-bool move(int x, int y, char sign){
+char tab[3][3];
+bool isMoveOK(int x, int y, char sign){
     if (x<0 or x >2 or y < 0 or y >2 ){
         return false;
     }
     if (tab[x][y] != ' '){
         return false;
     }
+    return true;
+}
+
+void move(int x, int y, char sign){
     tab[x][y] = sign;
 }
 
+void print(){
+    cout <<" _ _ _" <<endl;
+    for(int y=0; y<3; y++){
+        for (int x=0; x<3; x++){
+            cout << "|";
+            if(tab[x][y]==' ' ){
+                cout << "_";
+            }
+            else{
+                cout << tab[x][y];
+            }
+        }
+        cout << "|" <<endl;
+    }
+}
+
+bool isWinInRow(int y, char sign){
+    return tab[0][y]==sign and tab[1][y]==sign and tab[2][y]==sign;
+}
+bool isWinInCol(int x, char sign){
+    return tab[x][0]==sign and tab[x][1]==sign and tab[x][2]==sign;
+}
+bool isWinInDiagonal(char sign){
+    return (tab[0][0]==sign and tab[1][1]==sign and tab[2][2]==sign) or
+           (tab[2][0]==sign and tab[1][1]==sign and tab[0][2]==sign);
+}
+
+
+bool isEndOfGame(){
+    for(int i=0; i<3; i++){
+        if( isWinInRow( i,'o') or
+            isWinInRow( i,'x') or
+            isWinInCol( i,'o') or
+            isWinInCol( i,'x')){
+            return true;
+        }
+    }
+    return (isWinInDiagonal('o') or isWinInDiagonal('x'));
+}
+
+void init(){
+    for (int x=0; x<3; x++){
+        for(int y=0; y<3; y++){
+            tab[x][y]= ' ';
+        }
+    }
+}
 
 int main()
 {
-    int n;
-    cin >> n;
-    zad6(n);
+    init();
+    int x,y;
+    char sign ='x';
+    while(!isEndOfGame()){
+        print();
 
-    int a, b;
-    cout << "Podaj a: " <<endl;
-    cin >> a;
-    cout << "Podaj b: " <<endl;
-    cin >> b;
-    cout << odejmij(a,b)<<endl;
-    cout << pomnoz(a,b) <<endl;
-    cout << podziel(a,b) <<endl;
-    cout << dodaj(a,b)<<endl;
-    cout << "Inny komunikat!" << endl;
+        do{
+            cout << "Podaj x: " <<endl;
+            cin >> x;
+            cout << "Podaj y: " <<endl;
+            cin >> y;
+        } while(!isMoveOK(x,y,sign));
+        move(x,y,sign);
+        if(sign == 'x'){
+            sign = 'o';
+        }
+        else{
+            sign = 'x';
+        }
+    }
+
+    //TODO : wyswietl plansze i napisz kto wygral
+
 
 
     return 0;
